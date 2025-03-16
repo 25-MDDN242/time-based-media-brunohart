5c898901b2ce0a174e68ca8d29fea0a485a71189
brunohart <123848709+brunohart@users.noreply.github.com>
2025-03-16 15:32:05 +1300

    Made Change to ReadeMe after updating Maeda Clock Exercise

--- README.md
+++ README.md
@@ -8,4 +8,8 @@ Update this file as you go along to record your progress.
 # DAY ONE REALLY GETTING STRAPPED:
  Just opened the Project and really started trying to figure out what the hell is going on in the code base and generally -> life philosphy is just to keep moving forward putting one foot over the other and not looking back 
 
- Major Update with Phoebe
+Maeda clock was a lot of fun to start getting back into the zone for coding: been trying to find my feet and flow through all the tooling again and I found this exercise was the most helpful in converying the mechanics in my head in a more elegant way. 
+
+The Maeda Clock took inspiration from clock 2 and clock 9. I tried to figure out how to do a more gradual effect for adding a yellow dot every second and having it build up but it started getting very complicated with the framecount and going back to first year animation code and it was just getting messy so its a little jarring but the overall concept is communicated more simply in the code this way. 
+
+Esseantially there is a circle displaying the hour, minute and second with a rotating hand around the clock like in clock 9 and the background dots correspond to the build up in seconds as more and more are generated (supposed to be in realtion to the seconds)
--- maeda_clock.js
+++ maeda_clock.js
@@ -1,392 +1,58 @@
 // Update this function to draw you own maeda clock on a 960x500 canvas
 function draw_clock(obj) {
-   // YOUR MAIN CLOCK CODE GOES HERE
-   background(0); //  beige
-   fill(200); // dark grey
- 
-   let clockx = 70; // x and y postion of the clock 
-   let clocky = -60;
- 
-   // draw the numbers
-   function digit0(locx, locy) {
-   }
- 
-   function digit1(locx, locy) {
-   }
-   
-   function digit2(locx, locy) {
-   }
- 
-   function digit3(locx, locy) {
-   }
- 
-   function digit4(locx, locy) {
-   }
- 
-   function digit5(locx, locy) {
-   }
- 
-   function digit6(locx, locy) {
-   }
- 
-   function digit7(locx, locy) {
-   }
-   
-   function digit8(locx, locy) {
-   }
- 
-   function digit9(locx, locy) {
-   }
- 
-   function colon() {
-     circle(390 + clockx , 270 + clocky , 15);
-     circle(390 + clockx , 350 + clocky , 15);
-   }
- 
- // switches render the time in the correct format
- switch (obj.hours) {
-   case 0:
-     digit0(-160,0);
-     digit0(0,0);
-     break;
-   case 1:
-     digit0(0,0);
-     digit1(-160,0);
-     break;
-   case 2:
-     digit0(0,0);
-     digit2(-160,0);
-     break;
-   case 3:
-     digit0(0,0);
-     digit3(-160,0);
-     break;
-   case 4:
-     digit0(0,0);
-     digit4(-160,0);
-     break;
-   case 5:
-     digit0(0,0);
-     digit5(-160,0);
-     break;
-   case 6:
-     digit0(0,0);
-     digit6(-160,0);
-     break;
-   case 7:
-     digit0(0,0);
-     digit7(-160,0);
-     break;
-   case 8:
-     digit0(0,0);
-     digit8(-160,0);
-     break;
-   case 9:
-     digit0(0,0);
-     digit9(-160,0);
-     break;
-   case 10:
-     digit1(-160,0);
-     digit0(0,0);
-     break;
-   case 11:
-     digit1(-160,0);
-     digit1(0,0);
-     break;
-   case 12:
-     digit1(-160,0);
-     digit2(0,0);
-     break;
-   case 13:
-     digit1(-160,0);
-     digit3(0,0);
-     break;
-   case 14:
-     digit1(-160,0);
-     digit4(0,0);
-     break;
-   case 15:
-     digit1(-160,0);
-     digit5(0,0);
-     break;
-   case 16:
-     digit1(-160,0);
-     digit6(0,0);
-     break;
-   case 17:
-     digit1(-160,0);
-     digit7(0,0);
-     break;
-   case 18:
-     digit1(-160,0);
-     digit8(0,0);
-     break;
-   case 19:
-     digit1(-160,0);
-     digit9(0,0);
-     break;
-   case 20:
-     digit2(-160,0);
-     digit0(0,0);
-     break;
-   case 21:
-     digit2(-160,0);
-     digit1(0,0);
-     break;
-   case 22:
-     digit2(-160,0);
-     digit2(0,0);
-     break;
-   case 23:
-     digit2(-160,0);
-     digit3(0,0);
-     break;
- }
- 
- switch (obj.minutes) {
-   case 0:
-     digit0(260,0);
-     digit0(420,0);
-     break;
-   case 1:
-     digit0(260,0);
-     digit1(420,0);
-     break;
-   case 2:
-     digit0(260,0);
-     digit2(420,0);
-     break;
-   case 3:
-     digit0(260,0);
-     digit3(420,0);
-     break;
-   case 4:
-     digit0(260,0);
-     digit4(420,0);
-     break;
-   case 5:
-     digit0(260,0);
-     digit5(420,0);
-     break;
-   case 6:
-     digit0(260,0);
-     digit6(420,0);
-     break;
-   case 7:
-     digit0(260,0);
-     digit7(420,0);
-     break;
-   case 8:
-     digit0(260,0);
-     digit8(420,0);
-     break;
-   case 9:
-     digit0(260,0);
-     digit9(420,0);
-     break;
-   case 10:
-     digit1(260,0);
-     digit0(420,0);
-     break;
-   case 11:
-     digit1(260,0);
-     digit1(420,0);
-     break;
-   case 12:  
-     digit1(260,0);
-     digit2(420,0);
-     break;
-   case 13:
-     digit1(260,0);
-     digit3(420,0);
-     break;
-   case 14:
-     digit1(260,0);
-     digit4(420,0);
-     break;
-   case 15:
-     digit1(260,0);
-     digit5(420,0);
-     break;
-   case 16:
-     digit1(260,0);
-     digit6(420,0);
-     break;
-   case 17:
-     digit1(260,0);
-     digit7(420,0);
-     break;
-   case 18:
-     digit1(260,0);
-     digit8(420,0);
-     break;
-   case 19:
-     digit1(260,0);
-     digit9(420,0);
-     break;
-   case 20:
-     digit2(260,0);
-     digit0(420,0);
-     break;
-   case 21:  
-     digit2(260,0);
-     digit1(420,0);
-     break;
-   case 22:
-     digit2(260,0);
-     digit2(420,0);
-     break;
-   case 23:
-     digit2(260,0);
-     digit3(420,0);
-     break;
-   case 24:
-     digit2(260,0);
-     digit4(420,0);
-     break;
-   case 25:
-     digit2(260,0);
-     digit5(420,0);
-     break;
-   case 26:
-     digit2(260,0);
-     digit6(420,0);
-     break;
-   case 27:
-     digit2(260,0);
-     digit7(420,0);
-     break;
-   case 28:
-     digit2(260,0);
-     digit8(420,0);
-     break;
-   case 29:
-     digit2(260,0);
-     digit9(420,0);
-     break;
-   case 30:
-     digit3(260,0);
-     digit0(420,0);
-     break;
-   case 31:
-     digit3(260,0);
-     digit1(420,0);
-     break;
-   case 32:
-     digit3(260,0);
-     digit2(420,0);
-     break;
-   case 33:
-     digit3(260,0);
-     digit3(420,0);
-     break;
-   case 34:
-     digit3(260,0);
-     digit4(420,0);
-     break;
-   case 35:  
-     digit3(260,0);
-     digit5(420,0);
-     break;
-   case 36:
-     digit3(260,0);
-     digit6(420,0);
-     break;
-   case 37:
-     digit3(260,0);
-     digit7(420,0);
-     break;
-   case 38:
-     digit3(260,0);
-     digit8(420,0);
-     break;
-   case 39:
-     digit3(260,0);
-     digit9(420,0);
-     break;
-   case 40:
-     digit4(260,0);
-     digit0(420,0);
-     break;
-   case 41:
-     digit4(260,0);
-     digit1(420,0);
-     break;
-   case 42:
-     digit4(260,0);
-     digit2(420,0);
-     break;
-   case 43:
-     digit4(260,0);
-     digit3(420,0);
-     break;
-   case 44:  
-     digit4(260,0);
-     digit4(420,0);
-     break;
-   case 45:
-     digit4(260,0);
-     digit5(420,0);
-     break;
-   case 46:  
-     digit4(260,0);
-     digit6(420,0);
-     break;
-   case 47:
-     digit4(260,0);
-     digit7(420,0);
-     break;
-   case 48:
-     digit4(260,0);
-     digit8(420,0);
-     break;
-   case 49:
-     digit4(260,0);
-     digit9(420,0);
-     break;
-   case 50:
-     digit5(260,0);
-     digit0(420,0);
-     break;
-   case 51:
-     digit5(260,0);
-     digit1(420,0);
-     break;
-   case 52:
-     digit5(260,0);
-     digit2(420,0);
-     break;
-   case 53:
-     digit5(260,0);
-     digit3(420,0);
-     break;
-   case 54:
-     digit5(260,0);
-     digit4(420,0);
-     break;
-   case 55:
-     digit5(260,0);
-     digit5(420,0);
-     break;
-   case 56:
-     digit5(260,0);
-     digit6(420,0);
-     break;
-   case 57:
-     digit5(260,0);
-     digit7(420,0);
-     break;
-   case 58:
-     digit5(260,0);
-     digit8(420,0);
-     break;
-   case 59:
-     digit5(260,0);
-     digit9(420,0);
-     break;
- colon();
- }
- 
- colon();
- 
+
+  angleMode(DEGREES);
+
+  background(255);
+
+  // Get the current time
+  let H = hour();
+  let M = minute();
+  let S = second();
+
+  // Draw yellow background circles based on the current second value ---> the effect of this is that at each second that many number of yellow circles will be drawn at random but I had to slow it down a bit becuase it was lowkey overwhelming 
+  for (let i = 0; i < S; i++) { // Number of circles based on the current second
+    let x = random(width);
+    let y = random(height);
+    fill(255, 255, 0, 120); // Yellow with low transparency
+    noStroke();
+    ellipse(x, y, 30, 30);  // Draw background circle
+  }
+
+  // Draw the main circles (Time display)
+  fill(200); 
+  ellipse(width / 4, height / 2, 100, 100); // Hours
+  ellipse(width / 2, height / 2, 100, 100); // Minutes
+  ellipse(3 * width / 4, height / 2, 100, 100); // Seconds
+
+  // Display the time inside circles
+  fill(0);
+  textAlign(CENTER, CENTER);
+  textSize(40);
+  text(H, width / 4, height / 2);
+  text(M, width / 2, height / 2);
+  text(S, 3 * width / 4, height / 2);
+
+  // Convert time to degrees for rotating hands
+  let secondsToDegrees = map(S, 0, 59, 0, 360);
+  let minutesToDegrees = map(M, 0, 59, 0, 360);
+
+  // Draw rotating second hand
+  push();
+  translate(3 * width / 4, height / 2);
+  rotate(secondsToDegrees);
+  fill(255, 0, 0); // Red for visibility
+  rect(-5, -50, 10, 50);
+  pop();
+
+  // Draw rotating minute hand
+  push();
+  translate(width / 2, height / 2);
+  rotate(minutesToDegrees);
+  fill(0, 0, 255); // Blue for visibility
+  rect(-5, -40, 10, 40);
+  pop();
 }
+
+
+
